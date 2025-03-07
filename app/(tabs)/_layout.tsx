2dc285a123eb1425e0c30dbb5582776f721ac2f2
Rowoon <dev.rowoon@gmail.com>
2025-03-07 17:15:31 +0900

    feat: 탭바 디자인 처리

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,27 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: '일정' }} />
-      <Tabs.Screen name="myPage" options={{ title: '내정보' }} />
+      <Tabs.Screen 
+        name="index" 
+        options={{
+          title: '일정',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='calendar' size={size} color={color} /> 
+          )
+        }} 
+      />
+      <Tabs.Screen 
+        name="myPage" 
+        options={{
+          title: '내정보',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='person' size={size} color={color} /> 
+          )
+        }} 
+      />
     </Tabs>
   );
 }
