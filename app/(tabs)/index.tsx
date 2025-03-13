d9b8c5450b3cb914f46a2c1283a72267881e1153
Rowoon <dev.rowoon@gmail.com>
2025-03-13 15:31:15 +0900

    core: 리액트 네이티브 캘린더 라이브러리 설치

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 
 const Home: React.FC = () => {
   return (
-    <View>
+    <ScrollView>
       <Text>Home</Text>
-    </View>
+    </ScrollView>
   );
 };
 
